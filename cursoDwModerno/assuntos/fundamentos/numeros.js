c2ace3f062bd42e894e2c750c7c06ab9c3eddb42
pedro falcao <maximosjoselages@gmail.com>
2022-07-11 12:31:14 -0300

    2

---
+++ cursoDwModerno/assuntos/fundamentos/numeros.js
@@ -0,0 +1,9 @@
+const peso1 = 1.0
+const peso2 = Number('2.0')
+
+console.log(peso1,peso2)
+console.log(Number.isInteger(peso1)) //retorna como verdadeiro se o numero é inteiro
+console.log(Number.isInteger(peso2)) //retorna como verdadeiro se o numero é inteiro
+
+const avalicao1 =9.871
+const avalicao2 =6.871
